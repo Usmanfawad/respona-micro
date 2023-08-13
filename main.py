8b6b20566f6de2270b406f03d52183e0d6417165
Usman Fawad <ufawad0@gmail.com>
2023-08-13 08:28:07 +0500

    Deployment for the restapi completed!

--- main.py
+++ main.py
@@ -19,4 +19,14 @@ app.add_middleware(
 )
 
 
-from routes import *
+
+from fastapi_utils.tasks import repeat_every
+
+# Root route that runs every 30 seconds.
+@app.on_event("startup")
+@repeat_every(seconds=30)
+# Running after every 2 hours.
+# @repeat_every(seconds=60*120)
+@app.get("/")
+async def root():
+    return {"200": "success"}
